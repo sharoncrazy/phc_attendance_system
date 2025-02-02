f9708f6775cc7d95aa1ef68956d5ec32dee8c60d
Nalankumar <Nalankumar@users.noreply.github.com>
2025-02-02 21:45:29 +0530

    Initial commit

---
+++ backend/src/main/java/com/code_red/phc_attendance_system/PhcAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.code_red.phc_attendance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcAttendanceSystemApplication.class, args);
+	}
+
+}
